0b4d262fe079d158326aa770270eb188e9b87f5b
MedFarouk-ops <medfatouk55@gmail.com>
2021-12-07 01:20:14 +0100

    cart implementation

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -1,20 +1,109 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Livre;
+use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/cart", name="cart_")
+ */
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(SessionInterface $session, LivreRepository $livresRepository)
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        $panier = $session->get("panier", []);
+
+        // On "fabrique" les données
+        $dataPanier = [];
+        $total = 0;
+
+        foreach($panier as $id => $quantite){
+            $livre = $livresRepository->find($id);
+            $dataPanier[] = [
+                "livre" => $livre,
+                "quantite" => $quantite
+            ];
+            $total += $livre->getPrix() * $quantite;
+        }
+
+        return $this->render('cart/index.html.twig', compact("dataPanier", "total"));
+    }
+
+    /**
+     * @Route("/add/{id}", name="add")
+     */
+    public function add(Livre $livre, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $panier = $session->get("panier", []);
+        $id = $livre->getId();
+
+        if(!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+        }
+
+        // On sauvegarde dans la session
+        $session->set("panier", $panier);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    /**
+     * @Route("/remove/{id}", name="remove")
+     */
+    public function remove(Livre $livre, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $panier = $session->get("panier", []);
+        $id = $livre->getId();
+
+        if(!empty($panier[$id])){
+            if($panier[$id] > 1){
+                $panier[$id]--;
+            }else{
+                unset($panier[$id]);
+            }
+        }
+
+        // On sauvegarde dans la session
+        $session->set("panier", $panier);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Livre $livre, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $panier = $session->get("panier", []);
+        $id = $livre->getId();
+
+        if(!empty($panier[$id])){
+            unset($panier[$id]);
+        }
+
+        // On sauvegarde dans la session
+        $session->set("panier", $panier);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    /**
+     * @Route("/delete", name="delete_all")
+     */
+    public function deleteAll(SessionInterface $session)
+    {
+        $session->remove("panier");
+
+        return $this->redirectToRoute("cart_index");
     }
 }
